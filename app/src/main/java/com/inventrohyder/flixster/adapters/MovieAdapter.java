a4cdb6c2a867947507e58e3d178785b9601fb669
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-09-25 16:37:04 +0200

    ✨ feat: Create a MovieAdapter, RecyclerView.Adapter
    The MovieAdapter is used to pass data onto the RecyclerView

---
+++ app/src/main/java/com/inventrohyder/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,78 @@
+package com.inventrohyder.flixster.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.inventrohyder.flixster.R;
+import com.inventrohyder.flixster.models.Movie;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private final String TAG = getClass().getSimpleName();
+
+    Context mContext;
+    List<Movie> mMovies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        mContext = context;
+        mMovies = movies;
+    }
+
+    // Involves inflating the layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder");
+        View movieView = LayoutInflater.from(mContext).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+
+    // Usually involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: " + position);
+        // Get the movie at the passed in position
+        Movie movie = mMovies.get(position);
+        // Bind the movie data into the VH
+        holder.bind(movie);
+    }
+
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mTvTitle;
+        TextView mTvOverview;
+        ImageView mTvPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTvTitle = itemView.findViewById(R.id.tvTitle);
+            mTvOverview = itemView.findViewById(R.id.tvOverview);
+            mTvPoster = itemView.findViewById(R.id.tvPoster);
+        }
+
+        public void bind(Movie movie) {
+            mTvTitle.setText(movie.getTitle());
+            mTvOverview.setText(movie.getOverview());
+            Glide.with(mContext).load(movie.getPosterPath()).into(mTvPoster);
+        }
+    }
+}
